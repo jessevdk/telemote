9b2ac165f5541ecd065f55e0c352e2ea07418d63
Jesse van den Kieboom <jessevdk@gnome.org>
2011-08-02 12:45:34 +0200

    Fixed javascript includes

--- static/javascript/application.js
+++ static/javascript/application.js
@@ -1,6 +1,100 @@
-$.include('/static/javascript/utils.js');
-$.include('/static/javascript/listview.js');
-$.include('/static/javascript/playlists.js');
-$.include('/static/javascript/artists.js');
-$.include('/static/javascript/songs.js');
-$.include('/static/javascript/player.js');
+var initializing = true;
+
+function current_source()
+{
+	var selection = $('#playlists').playlists('listview').listview('selected');
+
+	if (selection)
+	{
+		return selection[0].id;
+	}
+}
+
+function animate_activate(elements, timeout, times, callback)
+{
+	if (times == 0)
+	{
+		callback(elements);
+		return;
+	}
+
+	elements.toggleClass('selected');
+
+	setTimeout(function () { animate_activate (elements, timeout, times - 1, callback)}, timeout);
+}
+
+$(document).ready(function () {
+	$('#playlists').playlists({
+		listview: {
+		    selection_changed: function () {
+		        $('#artists').artists('refresh', current_source());
+		    }
+		},
+		loaded: function () {
+		    $(this).playlists('listview').listview('select_index', 0);
+		}
+	});
+	$('#artists').artists({
+		listview: {
+		    selection_changed: function () {
+		        var src = current_source();
+		        var selection = $.map(this.listview('selected'),
+		                              function (item) {
+		                                  return item.id;
+		                              });
+
+		        $('#songs').songs('refresh', src, selection);
+		    }
+		},
+		loaded: function () {
+		    $(this).artists('listview').listview('select_index', 1);
+		}
+	});
+	$('#songs').songs({
+		listview: {
+		    activated: function (rows) {
+		        animate_activate(rows.target, 100, 6, function () {
+		            $('#player').player('queue', rows.data.id);
+		        });
+		    }
+		},
+		queued: function (row) {
+		    animate_activate(row.target, 100, 6, function () {
+		        $('#player').player('queue', row.data.id);
+		    });
+		}
+	});
+
+	$('#player').player({
+		setplaylist: function () {set_playlist($('#setplaylist'))}
+	});
+});
+
+function set_playlist(button)
+{
+	$(button).addClass('pressed');
+
+	var src = current_source();
+	var listview = $('#songs').songs('listview');
+	var selection = listview.listview('selected');
+
+	if (!selection || selection.length == 0)
+	{
+		selection = listview.listview('rows');
+	}
+
+	var ids = $.map(selection, function (item) {
+		return item.id;
+	});
+
+	setTimeout(function () {
+		$(button).removeClass('pressed');
+	}, 100);
+
+	$('#player').player('playlist', src, ids, function () {
+		$('#playlists').playlists('refresh');
+	});
+}
+
+
+/* vi:ex:ts=4 */
